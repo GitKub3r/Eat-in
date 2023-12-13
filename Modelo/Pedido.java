826bbbaa235d8b016d37481468bf2b838c9a0cab
GitKub3r <149192650+GitKub3r@users.noreply.github.com>
2023-12-13 18:49:53 +0100

    Se han creado las clases del Modelo

---
+++ Modelo/Pedido.java
@@ -0,0 +1,17 @@
+package Modelo;
+
+public class Pedido {
+	private String burger;
+	private String fries;
+	private String drink;
+	private double price;
+	
+	public Pedido(String burger, String fries, String drink, double price) {
+		this.burger = burger;
+		this.fries = fries;
+		this.drink = drink;
+		this.price = price;
+	}
+	
+	
+}
