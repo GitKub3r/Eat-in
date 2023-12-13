9a8773df38c5ed989f1a74771289b2b4a101ed5f
GitKub3r <149192650+GitKub3r@users.noreply.github.com>
2023-12-13 19:15:15 +0100

    Se ha realizado el controlador para manejar la creación del pedido

--- Eat-In/src/Controlador/MainClass.java
+++ Eat-In/src/Controlador/MainClass.java
@@ -1,8 +1,38 @@
 package Controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Modelo.*;
+
+import Vista.*;
+
 public class MainClass {
 	
 	public static void main(String [] args) {
+		Loading load = new Loading();
+		load.main(args);
 		
+		Window window = new Window();
+		window.setVisible(true);
+		
+		
+	}
+	
+	public void hacerPedido(Pedido p, Direccion d) {
+		System.out.println("PEDIDO: BURGER KING - MENÚ BÁSICO");
+		System.out.println("---------------------------------");
+		System.out.println("Hamburgesa: " + p.getBurger());
+		System.out.println("Complemento: " + p.getFries());
+		System.out.println("Bebida: " + p.getDrink());
+		System.out.println("---------------------------------");
+		System.out.println("PRECIO: " + p.getPrice());
+		System.out.println("---------------------------------");
+		System.out.println("DIRECCIÓN DE ENTREGA");
+		System.out.println("Calle: " + d.getStreet());
+		System.out.println("Portal: " + d.getBuilding());
+		System.out.println("Piso: " + d.getFloor());
+		System.out.println("Puerta: " + d.getDoor());
+		System.out.println("---------------------------------");
 	}
 }
